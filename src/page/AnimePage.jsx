30566f25764469f600af1b873ca0ded8771a54b9
Alfian Nahar <58506749+alfianahar@users.noreply.github.com>
2022-06-29 11:48:41 +0700

    fix key, and get pathname of page

--- src/page/AnimePage.jsx
+++ src/page/AnimePage.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { matchRoutes, useLocation } from 'react-router-dom'
+
+const usePathname = () => {
+    const location = useLocation();
+    return location.pathname;
+}
 
 const AnimePage = () => {
-
+    const currentPath = usePathname().substring(
+        usePathname().indexOf("e/") + 1,
+        usePathname().lastIndexOf("/")
+    ).replace('/', '');
+    // console.log(currentPath)
     return (
         <div>Anime Detail Page</div>
     )
 }
 
 export default AnimePage
+

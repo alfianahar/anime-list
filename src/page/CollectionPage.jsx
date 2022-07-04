a3e7f5ea6a1f45b60eb7f106f4de3a493a44a2a5
Alfian Nahar <58506749+alfianahar@users.noreply.github.com>
2022-07-05 02:26:28 +0700

    add collection page list

--- src/page/CollectionPage.jsx
+++ src/page/CollectionPage.jsx
@@ -1,3 +1,6 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+import styled from '@emotion/styled';
 import React, { useEffect, useState } from 'react'
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
@@ -15,6 +18,11 @@ const usePathname = () => {
     return location.pathname;
 }
 
+const ListContainer = styled('div')`
+    padding-left: 1rem;
+    padding-right: 1rem;    
+`
+
 const CollectionPage = () => {
 
     const currentPath = usePathname().substring((usePathname().lastIndexOf("/")
@@ -22,98 +30,136 @@ const CollectionPage = () => {
     const colPath = currentPath
     // console.log(colPath)
 
-    const [id, setId] = useState([])
-    const [list, setList] = useState([])
+    const [details, setDetails] = useState()
+    const [list, setList] = useState()
 
 
 
     // const docRef = doc(db, 'users', 'user1');
     // const colNameRef = collection(db, 'users', 'user1', 'colName');
     const animeListRef = collection(db, 'users', 'user1', 'animeList');
+    const q = query(animeListRef, where("colName", "==", colPath));
 
+    // const getId = async () => {
+    //     let animes = [];
+    //     await onSnapshot(q, (querySnapshot) => {
+    //         querySnapshot.forEach((doc) => {
+    //             animebyId(({ ...doc.data(), id: doc.id }.animeId)).then((response) => animes.push(response.media[0]));
+    //             // animes.push({ ...doc.data(), id: doc.id }.animeId)
+    //             // .map((i) => (
+    //             //     animebyId(i.animeId).then((response) => setId(response.media))
+    //             // ))
+    //             // console.log({ ...doc.data(), id: doc.id }.animeId)
+    //             // setList(animes)
+    //         });
+    //         // console.log(animes.length)
 
-    useEffect(() => {
-        const q = query(animeListRef, where("colName", "==", colPath));
-
-        const getId = () => {
-            onSnapshot(q, (querySnapshot) => {
-                const animes = [];
-                querySnapshot.forEach((doc) => {
-                    // animebyId(doc.data().animeId).then((response) => animes.push(response.media[0]))
-                    animes.push(doc.data().animeId);
-                })
-                setId(animes)
-            })
-        }
-        getId()
-
-    }, []);
-    console.log(id)
+    //     })
+    //     console.log(animes)
+    //     // setList(animes)
+    // }
+    // const getId = async () => {
+    //     // Snapshoot returns Unsubscribe to prevent from memoryleaks you need to ivoke it later like snap()
+    //     const snap = onSnapshot(q, async (querySnapshot) => {
+    //         if (querySnapshot.empty) return // If there is no data just do nothing.
+    //         let promises = []
+    //         for (let doc of querySnapshot.docs) {
+    //             // Do all promisess in parallel
+    //             promises.push(animebyId(({ ...doc.data(), id: doc.id }.animeId)))
+    //         }
+    //         // Await all promises
+    //         const results = await Promise.all(promises)
+    //         if (!results.length) return // If no result abort.
+    //         let animes = []
+    //         for (let doc of results) {
+    //             animes.push(doc.media[0]);
+    //         }
+    //         setList(animes);
+    //     })
+    // }
 
     // useEffect(() => {
-    //     id.map((anime) => (
-    //         animebyId(anime).then((response) => setList(response.media))
-    //     ))
+
+    //     getId()
+
     //     console.log(list)
-    // }, [])
+    //     // eslint-disable-next-line react-hooks/exhaustive-deps
+    // }, []);
+    // console.log(list)
+    // console.log(list.length)
+
+    useEffect(() => {
+
+        const getId = async () => {
+            onSnapshot(q, async (querySnapshot) => {
+                let animes = [];
+                querySnapshot.forEach(doc => {
+                    const anime = { ...doc.data(), id: doc.id };
+                    animes.push(anime);
+                })
+
+                const animesNew = [];
+                for (let anime of animes) {
+                    const response = await animebyId(anime.animeId);
+                    animesNew.push(response.media[0]);
+                }
+                setList(animesNew);
+            })
+        }
+
+        getId()
+        console.log(list)
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return (
-        <div>
-            {/* <img src={list.coverImage.extraLarge} alt="sdasd" /> */}
-            {/* <ImageList sx={{ width: 500, height: 450 }}>
-                <ImageListItem key="Subheader" cols={2}>
-                    <ListSubheader component="div">{colPath}</ListSubheader>
-                </ImageListItem>
-                {id.map((item) => (
-                    <ImageListItem >
-                        <img
-                            src={`${item.coverImage.extraLarge}?w=248&fit=crop&auto=format`}
-                            srcSet={`${item.coverImage.extraLarge}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                            alt={item.title.english ? item.title.english : item.title.romaji}
-                            loading="lazy"
-                        />
-                        <ImageListItemBar
-                            title={item.title}
-                            actionIcon={
-                                <IconButton
-                                    sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                                    aria-label={`info about ${item.title.english ? item.title.english : item.title.romaji}`}
-                                >
-                                    <InfoIcon />
-                                </IconButton>
-                            }
-                        />
-                    </ImageListItem>
-                ))}
-            </ImageList> */}
+        <>
 
-            {/* <input
-                placeholder='Name...'
-                onChange={(e) => {
-                    setNewName(e.target.value)
-                }}
-            />
-            <input
-                type='number'
-                placeholder='Age...'
-                onChange={(e) => {
-                    setNewAge(e.target.value)
-                }}
-            />
-            <button onClick={createUser}> Create User </button>
-            {users.map((user) => {
-                return (
-                    <div>
-                        {' '}
-                        <h1>Name: {user.name}</h1>
-                        <h1>Age: {user.age}</h1>
-                        <button onClick={() => { updateUser(user.id, user.age) }}> Tambah Umur </button>
-                        <button onClick={() => { deletUser(user.id) }}> Delete User </button>
-                    </div>
+            {list === undefined ?
+                <>still Loading.....</> :
 
-                )
-            })} */}
-        </div>
+                <>
+
+                    {/* <p>{list}</p> */}
+                    <ListContainer>
+
+                        <ImageList sx={{ width: '100%', height: '100%' }}>
+                            <ImageListItem key="Subheader" cols={2}>
+                                <ListSubheader component="div">{colPath}</ListSubheader>
+                            </ImageListItem>
+                            {list.map((item) => (
+                                <>
+                                    <ImageListItem key={item.id}>
+                                        <img
+                                            src={`${item.coverImage.extraLarge}?w=248&fit=crop&auto=format`}
+                                            srcSet={`${item.coverImage.extraLarge}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                                            alt={item.title.english ? item.title.english : item.title.romaji}
+                                            loading="lazy"
+                                        />
+                                        <ImageListItemBar
+                                            title={item.title.english ? item.title.english : item.title.romaji}
+                                            subtitle={item.author}
+                                            actionIcon={
+                                                <IconButton
+                                                    sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                                                    aria-label={`info about ${item.title.english ? item.title.english : item.title.romaji}`}
+                                                >
+                                                    <InfoIcon />
+                                                </IconButton>
+                                            }
+                                        />
+                                    </ImageListItem>
+                                </>
+                            )
+                            )}
+                        </ImageList>
+                    </ListContainer>
+
+                </>
+
+            }
+        </>
     )
 }
 
